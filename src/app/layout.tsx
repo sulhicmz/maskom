b5cb4738bf7d7a1a66b74124f16e1a69f1d51594
sulhi.cmz <sulhi.cmz@gmail.com>
2025-09-19 17:46:57 +0000

    fix: syntax error in Sidebar.tsx and React hooks dependency
    - Fix syntax error in src/components/dashboard/Sidebar.tsx by adding displayName to React.memo component
    - Fix React Hook useEffect missing dependency in src/components/common/ScrollToTop.tsx by adding showScroll to dependency array
    - Clean up encoding issues in README.md
    - Add PROBLEMS.md to document all identified issues and fixes
    
    All lint errors resolved, build successful

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         {/* For IE  */}
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300;400;500;600;700&display=swap" />
+        {/* Font loaded via next/font instead */}
       </head>
       <body suppressHydrationWarning={true}>
         {children}
