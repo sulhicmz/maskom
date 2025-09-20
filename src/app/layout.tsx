f7046f695391e6c791906bc4a8f6ba3f1c5a34f9
Sulhi <22936450+sulhicmz@users.noreply.github.com>
2025-09-20 07:31:27 +0700

    Configure Cloudflare Worker deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import "../styles/index.scss";
 
 export default function RootLayout({
@@ -16,8 +17,13 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         {/* For IE  */}
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@400;500;600;700&display=swap"
+        />
         <link rel="icon" href="/favicon.png" sizes="any" />
-        {/* Font loaded via next/font instead */}
       </head>
       <body suppressHydrationWarning={true}>
         {children}
